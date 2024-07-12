ac06fed2ae0dd909ec03dc844eb7943ec6a9976c
William Mallett <mallett002@gmail.com>
2024-07-12 06:40:55 -0500

    cleanup :broom:

--- config/openai.js
+++ config/openai.js
@@ -3,10 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const configuration = new Configuration({
-//     apiKey: 
-// });
-
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
